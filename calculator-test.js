3599135c6c24b508bffb4f30ad7a2427b8be49f8
Yarkin Reis <yarkin94@gmail.com>
2024-01-10 12:18:29 -0800

    test.js codes are added and tested

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,14 @@
 
 it('should calculate the monthly rate correctly', function () {
-    // ...
+    let values = {amount : 5000,years : 5, rate:3.4 };
+    expect (calculateMonthlyPayment(values)).toEqual("90.73");
   });
   
   
   it("should return a result with 2 decimal places", function() {
     // ..
+    let values = {amount : 5000,years : 5, rate:3.4 };
+    expect (calculateMonthlyPayment(values)).toEqual("90.73");
   });
   
   /// etc
